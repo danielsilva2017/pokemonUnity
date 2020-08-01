300bf89642bb74beb3d957f65df060761e0cdf21
danielsilva2017 <silvadaniel@ua.pt>
2020-08-01 17:37:42 +0100

    First commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake(){
+        Debug.Log("here");
+        animator = GetComponent<Animator>();
+    }
+    private void Update(){
+        if(!isMoving){
+            input.x=Input.GetAxisRaw("Horizontal");
+            input.y=Input.GetAxisRaw("Vertical");
+            Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+            if(input.x !=0)input.y=0;
+
+            if(input != Vector2.zero){
+               
+                animator.SetFloat("moveX",input.x);
+                animator.SetFloat("moveY",input.y);
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+                StartCoroutine(Move(targetPos));
+            }
+        }
+
+        //animator.SetBool("isMoving",isMoving);
+    }
+    //Inumerator is used to do something over a period of time -  move current to target pos over a period of time
+
+    IEnumerator Move(Vector3 targetPos){
+        isMoving=true;
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,targetPos,moveSpeed* Time.deltaTime);
+            
+            yield return null;
+        }
+        transform.position= targetPos;
+        isMoving=false;
+    }
+}
---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu: MonoBehaviour {
+
+    public void PlayGame () 
+    {
+        Debug.Log("xd");
+        SceneManager.LoadScene (1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log ("QUIT!");
+        Application.Quit(); 
+    }
+
+}
