33d32cf0488e3dec630646c0ed38cca7d85de529
danielsilva2017 <silvadaniel@ua.pt>
2020-08-02 11:16:38 +0100

    colliders added

---
+++ Assets/camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public Transform player;
+    public float cameraDistance=30.0f;
+    public void Awake(){
+        GetComponent<UnityEngine.Camera>().orthographicSize=((Screen.height/2)/cameraDistance );
+    }
+    public void FixedUpdate(){
+        transform.position= new Vector3(player.position.x,player.position.y,player.position.z);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
