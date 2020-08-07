b8b8014cb7989cb996e18a13f5ddf945b0c653df
danielsilva2017 <silvadaniel@ua.pt>
2020-08-07 14:02:54 +0100

    pokecenter created

---
+++ Assets/PokeCenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PokeCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(){
+        SceneManager.LoadScene(4);
+    }
+}
